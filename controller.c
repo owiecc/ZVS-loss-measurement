6e38543ec8ff93ce659ca845df1ad4c49c1d07e5
Szymon Bęczkowski <szymon.beczkowski@me.com>
2023-01-05 14:08:17 +0100

    fix function name

--- controller.c
+++ controller.c
@@ -57,7 +57,7 @@ __interrupt void adcA1ISR(void)
     float u1 = updatePI(&PI_ILhi, errILhi);
     float u2 = updatePI(&PI_ILlo, errILlo);
 
-    //updateModulator(1.0f - u1 - u2, u2);
+    updateEPWM(1.0f - u1 - u2, u2);
 
     AdcaRegs.ADCINTFLGCLR.bit.ADCINT1 = 1; // Clear the interrupt flag
 
