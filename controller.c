4441ae865e31f07fdae98d95d6d49700814f07cc
Szymon Bęczkowski <szymon.beczkowski@me.com>
2023-02-01 14:44:27 +0100

    update PI controllers

--- controller.c
+++ controller.c
@@ -5,10 +5,11 @@
 #include "pwm.h"
 #include "leds.h"
 
-#define PI_ILhi_Ki 50.0f
+#define PI_ILhi_Ki 10.0f
 #define PI_ILlo_Ki 0.05f
+#define HARD_CYCLE_LIMIT 20000 // PWM off
 
-static struct piController PI_ILhi = {0, 0, 0, 0, 0}; // ILhi controller
+static struct piController PI_DeltaIL = {0, 0, 0, 0, 0}; // ILhi controller
 static struct piController PI_ILlo = {0, 0, 0, 0, 0}; // ILlo controller
 
 static float refILhi = 0.0f; // value overwritten in main.c
@@ -16,8 +17,8 @@ static float refILlo = -5.0f;
 
 void initPIConttrollers(void)
 {
-    PI_ILhi = initPI(PI_ILhi_Ki/FSW, 2*PI_ILhi_Ki/FSW, 0.5, -1, 0.02, 0.48, -0.4);
-    PI_ILlo = initPI(PI_ILlo_Ki/FSW, 2*PI_ILlo_Ki/FSW, 0.5, -1, 0.00, 0.4, -0.4);
+    PI_DeltaIL = initPI(PI_ILhi_Ki/FSW, 2*PI_ILhi_Ki/FSW, 0.5, -1, 0.02, 0.45, 0);
+    PI_ILlo = initPI(PI_ILlo_Ki/FSW, 2*PI_ILlo_Ki/FSW, 0.5, -1, 0.00, 0.45, -0.45);
 }
 
 void setControllerILRef(float x) {
@@ -35,19 +36,22 @@ __interrupt void adcA1ISR(void)
 {
     struct ADCResult meas = scaleADCs();
 
-    //static unsigned int ncycles = 0;
+    static unsigned int ncycles = 0;
 
-    //ncycles++;
-    //if (ncycles >= HARD_CYCLE_LIMIT) // trip the converter
-    //{
-    //    disablePWM();
-    //    ncycles = 0;
-    //}
+    ncycles++;
+    if (ncycles >= HARD_CYCLE_LIMIT) // trip the converter
+    {
+        disablePWM();
+        ncycles = 0;
+    }
 
-    float errILhi = refILhi - meas.ILhi; // TODO fix measured values
+    //meas.ILhi = 0.0f;
+    //meas.ILlo = 0.0f;
+
+    float errDeltaIL = (refILhi - refILlo) - (meas.ILhi - meas.ILlo);
     float errILlo = refILlo - meas.ILlo;
 
-    float u1 = updatePI(&PI_ILhi, errILhi);
+    float u1 = updatePI(&PI_DeltaIL, errDeltaIL);
     float u2 = updatePI(&PI_ILlo, errILlo);
 
     updateEPWM(1.0f - u1 - u2, u1);
